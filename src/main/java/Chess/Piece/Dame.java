73ee7e84e45cd23c29aa913e1f5ddfd0254ecf88
SERRE, Lucas <146104634+SERRE-Lucas-2326100aa@users.noreply.github.com>
2024-06-03 11:54:25 +0200

    Update Dame.java

--- src/main/java/Chess/Piece/Dame.java
+++ src/main/java/Chess/Piece/Dame.java
@@ -1,4 +1,49 @@
-package Chess.Piece;
+package Piece;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Dame {
+
+    int PosX;
+    int PosY;
+    String Couleur;
+    public Dame (Couleur, PosX, PosY){
+        super(Couleur,PosX,PosY);
+        ArrayList<String> move = new ArrayList<>();
+    }
+
+    public List<String> getFields() {
+        List<String> fields = new ArrayList<>();
+        int x = PosX;
+        int y= PosY;
+
+        for (int i = 1; PosX + i < 8 && PosY + i < 8; i++)
+            if (addField(PosX + 1, PosY + 1, fields))
+                break;
+        for (int i = 1; PosX + i < 8 && PosY - i >= 0; i++)
+            if (PosX + 1, PosY - 1)
+                break;
+        for (int i = 1; PosX - i >= 0 && PosY - i >= 0; i++)
+            if (PosX + 1, PosY + 1)
+                break;
+        for (int i = 1; PosX - i >= 0 && PosY + i < 8; i++)
+            if (PosX - 1, PosY + 1)
+                break;
+        for (int i = 1; PosX + i < 8; i++)
+            if (PosX - 1)
+                break;
+        for (int i = 1; PosX - i >= 0; i++)
+            if (PosX - 1)
+                break;
+        for (int i = 1; PosY + i < 8; i++)
+            if (PosY + 1)
+                break;
+        for (int i = 1; PosY - i >= 0; i++)
+            if (PosY - 1)
+                break;
+        return fields;
+    }
+
+
 }
