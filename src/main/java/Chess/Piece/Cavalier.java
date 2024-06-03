d8b69c4ec4b0e266810cdf54623e32c0febc7899
LY-Yoann-2326070 <146416536+LY-Yoann-2326070ab@users.noreply.github.com>
2024-06-03 11:55:56 +0200

    Update Cavalier.java
    Ajout du cavalier (en attente du plateau pour continuer)

--- src/main/java/Chess/Piece/Cavalier.java
+++ src/main/java/Chess/Piece/Cavalier.java
@@ -1,4 +1,25 @@
-package Chess.Piece;
+package Piece;
+
+
+import java.util.ArrayList;
+
 
 public class Cavalier {
+
+    public Cavalier(Couleur color, ChessMain ) {
+        super(color, "Cavalier", field);
+    }
+
+    @Override
+    public List<> getAccessibleFields() {
+        List<ChessMain> fields = new ArrayList<>();
+        addField(x + 2, y + 1, fields);
+        addField(x + 2, y - 1, fields);
+        addField(x + 1, y + 2, fields);
+        addField(x + 1, y - 2, fields);
+        addField(x - 2, y + 1, fields);
+        addField(x - 2, y - 1, fields);
+        addField(x - 1, y + 2, fields);
+        addField(x - 1, y - 2, fields);
+        return fields;
 }
