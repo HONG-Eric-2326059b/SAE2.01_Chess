c905c7cef675b51b3eb92a05a58d052a08987db5
h23005461 <eric.hong@etu.univ-amu.fr>
2024-06-03 11:14:41 +0200

    Pion

--- src/main/java/Chess/Piece/Pion.java
+++ src/main/java/Chess/Piece/Pion.java
@@ -7,4 +7,26 @@ import java.util.Optional;
 
 public class Pion {
 
+    int PosX;
+    int PosY;
+    String Couleur;
+    public Pion(Couleur,PosX,PosY){
+        super(Couleur,PosX,PosY);
+        ArrayList<String> move = new ArrayList<>();
+    }
+
+    public void MoveCondition(){
+        int x = PosX;
+        int y = PosY;
+        if(Couleur == White){
+            if(PosY == 6){
+
+            }
+        }
+        if(Couleur == Black){
+            PosY = PosY +1;
+
+        }
+
+    }
 }
