ce694d04fee85f147a1d45dbe4ef7784ffcd052b
h23005461 <eric.hong@etu.univ-amu.fr>
2024-06-03 11:52:20 +0200

    Couleur

--- src/main/java/Chess/Piece/Couleur.java
+++ src/main/java/Chess/Piece/Couleur.java
@@ -2,5 +2,16 @@ package Chess.Piece;
 
 public class Couleur {
 
+    public String BLACK;
+    public String White;
+
+    private String Couleur;
+
+    Color(String Couleur){
+        this.Couleur = Couleur;
+    }
+    public String getCouleur(){
+        return Couleur;
+    }
 
 }
