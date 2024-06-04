200ef5e0d5e6527a74a62b6c122825ca3e32cf69
h23005461 <eric.hong@etu.univ-amu.fr>
2024-06-04 15:30:31 +0200

    fxml

--- src/main/java/Chess/ChessMain.java
+++ src/main/java/Chess/ChessMain.java
@@ -6,17 +6,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ChessMain extends Application {
-
-    @FXML GridPane
-    @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("Chess.fxml"));
         Scene scene = new Scene(loader.load());
         stage.setTitle("Chess");
         stage.setScene(scene);
         stage.show();
-    }
-    public static void main(String[] args) {
-        launch(args);
     }
 }
