1b93efb2ac4945edd916341ecdfc4d1953cf95b5
h23005461 <eric.hong@etu.univ-amu.fr>
2024-06-05 11:56:28 +0200

    Boutton Menu

--- src/main/java/Chess/Piece/MovePiece.java
+++ src/main/java/Chess/Piece/MovePiece.java
@@ -1,4 +1,13 @@
 package Chess.Piece;
+import Chess.ChessMain;
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 
 public class MovePiece {
+
 }
